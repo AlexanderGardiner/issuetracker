142df535caa9d2ee2ecf9cee253d0f221a3031b1
Alex Gardiner <misc@alexandergardiner.com>
2022-09-03 15:41:40 -0700

    Serve static files

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
+const path = require('path')
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
-app.use( express.static( __dirname + '/public' ) );
-app.listen(80);
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.listen(8000);
+console.log("Server Running")
