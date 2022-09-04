fc88af77703459f0b24706c665ee5abfb5f8b0cd
Alex Gardiner <misc@alexandergardiner.com>
2022-09-03 20:52:24 -0700

    working order of database operation

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/IssueTracker";
 const PORT = 8080;
-
+var MongoDatabase;
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use( express.static( __dirname + '/public' ) );
 app.use(bodyParser.json());
@@ -14,43 +14,46 @@ app.listen(PORT, function(err){
     console.log("Server listening on PORT", PORT);
 });
 
+async function startupDatabase() {
+    console.log("Starting Database");
+    MongoDatabase = await MongoClient.connect(url);
+}
+
 async function createNewProject(projectName) {
-    MongoClient.connect(url, function(err, db) {
+    console.log("Creating New Project");
+    let dbo = MongoDatabase.db("IssueTracker");
+    await dbo.createCollection(projectName, function(err, res) {
         if (err) throw err;
-        let dbo = db.db("IssueTracker");
-        dbo.createCollection(projectName, function(err, res) {
-          if (err) throw err;
-          console.log("Project Created");
-          db.close();
-        });
-    }); 
+        console.log("Project Created");
+        
+    });
 }
 
 async function createNewIssue(projectName,issueData) {
-    MongoClient.connect(url, async function(err, db) {
+    console.log("Creating New Issue");    
+    let dbo = MongoDatabase.db("IssueTracker");
+    let id = await dbo.collection(projectName).countDocuments();
+    issueData.id = id.toString();
+    await dbo.collection(projectName).insertOne(issueData, function(err, res) {
         if (err) throw err;
-        let dbo = db.db("IssueTracker");
-        let id = await dbo.collection(projectName).countDocuments();
-        issueData.id = id.toString();
-        dbo.collection(projectName).insertOne(issueData, function(err, res) {
-          if (err) throw err;
-          console.log("Issue Created");
-          db.close();
-        });
-    }); 
+    });
+    
 }
 
 async function editIssue(projectName,propertyID,propertyName,propertyData) {
-    MongoClient.connect(url, function(err, db) {
+    console.log("Editing Issue");
+    let dbo = MongoDatabase.db("IssueTracker");
+    await dbo.collection(projectName).updateOne({id:propertyID},{ $set: { [propertyName]: propertyData } }, function(err, res) {
         if (err) throw err;
-        let dbo = db.db("IssueTracker");
-        dbo.collection(projectName).updateOne({id:propertyID},{ $set: { [propertyName]: propertyData } }, function(err, res) {
-          if (err) throw err;
-          console.log("Issue Edited");
-          db.close();
-        });
-    }); 
+    });
 }
-//createNewProject("Project2");
-createNewIssue("Project2",{"title":"Is a bug","status":"started"})
-editIssue("Project2","1","status","done")
+
+async function main() {
+    await startupDatabase();
+    //createNewProject("Project2");
+    await createNewIssue("Project2",{"title":"Is a bug","status":"started"})
+    await editIssue("Project2","2","status","done")
+}
+main();
+
+
