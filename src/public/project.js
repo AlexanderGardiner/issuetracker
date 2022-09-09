7214c0159a4b37fe80009443bab3b057922e5b65
Alex Gardiner <misc@alexandergardiner.com>
2022-09-08 20:19:04 -0700

    Different types of issues displayed

--- src/public/project.js
+++ src/public/project.js
@@ -25,14 +25,15 @@ function displayProject(data) {
   document.getElementById("title").innerHTML = projectName;
   
   let properties = [];
-
-  let header;
   let schemaKeys = Object.keys(data.schema)
+
+  // Vars for headers
+  let header;
   let headerRow = [];
   let headerRowInputs = [];
 
+  // Create ID header
   header = (table.insertRow(-1));
-
   headerRow.push(header.insertCell(0));
   headerRowInputs.push(document.createElement("h1"));
   headerRowInputs[0].innerHTML =  "ID";
@@ -40,6 +41,7 @@ function displayProject(data) {
 
   headerRow[0].appendChild(headerRowInputs[0])
 
+  // Create other headers
   for (let i=1;i<schemaKeys.length+1;i++) {
     
     headerRow.push(header.insertCell(i));
@@ -64,26 +66,75 @@ function displayProject(data) {
 
     // Create columns based on schema and populate with data
     for (let j=0;j<projectKeys.length;j++) {
-      propertiesInRow.push(properties[i].insertCell(j));
-      propertiesInRowInput.push(document.createElement("Input"));
-      propertiesInRowInput[j].setAttribute("type", "text");
+      
+      
       if (j==0) {
+        // Create ID cells
+        propertiesInRow.push(properties[i].insertCell(j));
+        propertiesInRowInput.push(document.createElement("Input"));
+        propertiesInRowInput[j].setAttribute("type", "text");
         propertiesInRowInput[j].setAttribute("readonly", "true");
+        propertiesInRowInput[j].setAttribute("value", JSON.stringify(data.project[i][projectKeys[j]]).replaceAll('"', ''));
+      } else {
+        // Create other types of cells
+        let typeOfCell = data.schema[schemaKeys[j-1]].type;
+        if (typeOfCell=="Text") {
+          // Create cell if it's type is text
+          propertiesInRow.push(properties[i].insertCell(j));
+          propertiesInRowInput.push(document.createElement("Input"));
+          propertiesInRowInput[j].setAttribute("type", "text");
+          propertiesInRowInput[j].setAttribute("value", JSON.stringify(data.project[i][projectKeys[j]]).replaceAll('"', ''));
+        } else if (typeOfCell=="Time") {
+          // Create cell if it's type is time
+          propertiesInRow.push(properties[i].insertCell(j));
+          propertiesInRowInput.push(document.createElement("Input"));
+          propertiesInRowInput[j].setAttribute("type", "text");
+          propertiesInRowInput[j].setAttribute("readonly", "true");
+          propertiesInRowInput[j].setAttribute("value", JSON.stringify(data.project[i][projectKeys[j]]).replaceAll('"', ''));
+        } else if (typeOfCell=="Multiple Choice") {
+          // Create cell if it's type is multiple choice
+          let options = data.schema[schemaKeys[j-1]].options;
+          let selectedIndex = 0;
+          propertiesInRow.push(properties[i].insertCell(j));
+          propertiesInRowInput.push(document.createElement("Select"));
+          propertiesInRowInput[j].setAttribute("value", JSON.stringify(data.project[i][projectKeys[j]]).replaceAll('"', ''));
+          for (let k = 0; k < options.length; k++) {
+            if (options[k]==data.project[i][projectKeys[j]]) {
+              selectedIndex = k;
+            }
+            let option = document.createElement("option");
+            option.value = options[k];
+            option.text = options[k];
+            propertiesInRowInput[j].appendChild(option);
+          }
+          propertiesInRowInput[j].selectedIndex = selectedIndex;
+        } else if (typeOfCell=="User") {
+          // Create cell if it's type is user
+          propertiesInRow.push(properties[i].insertCell(j));
+          propertiesInRowInput.push(document.createElement("Input"));
+          propertiesInRowInput[j].setAttribute("type", "text");
+          propertiesInRowInput[j].setAttribute("value", JSON.stringify(data.project[i][projectKeys[j]]).replaceAll('"', ''));
+          // TODO: link to users when setup login system
+        }
       }
       
-      propertiesInRowInput[j].setAttribute("value", JSON.stringify(data.project[i][projectKeys[j]]).replaceAll('"', ''));
+      
+      //TODO: Add project submission/updation
+      
+      // Add cells to correct class and add to table
       propertiesInRowInput[j].classList.add("tableinput");
 
       propertiesInRow[j].appendChild(propertiesInRowInput[j])
     }
       
   }
-  // TODO: need to set up other input types for issues (multiple choice etc)
 
+
+  // Set width of td to be size of page
   let tdElements = document.getElementsByTagName("td");
 
   for(let i = 0; i < tdElements.length; i++) {
-    console.log(tdElements[i])
-    tdElements[i].style.width = "500px"//(window.innerWidth/tdElements.length - 10).toString() + "px";
+    tdElements[i].style.width = "1000px";
   }
 }
+
