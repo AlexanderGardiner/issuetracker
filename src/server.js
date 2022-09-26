3a7155d12ad01e0824d0c5181c2565e9e1209b67
Alex Gardiner <99229221+AlexanderGardiner@users.noreply.github.com>
2022-09-26 03:53:31 +0000

    style updates

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
-const process = require('process')
-process.chdir("src")
-
 // Load required libraries
+// Process for changing working directory
 // Express for routing
 // Body-parser for parsing the body
 // Mongodb for interfacing with the database
-
+const process = require('process')
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser')
@@ -13,145 +11,142 @@ const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 const fs = require('fs');
 
+// Change working directory
+process.chdir("src");;
 
 // Vars for mongodb database
 var url = "mongodb+srv://Main:8dmfv2tXNor2HG9T@issuetracker.9w0hzlx.mongodb.net/?retryWrites=true&w=majority";
 var MongoDatabase;
 
+// Start express server
 async function startExpressServer() {
-  // Start express server
+  // Define port and set up static files and body parser
   const PORT = 8080;
-  app.use(bodyParser.urlencoded({ extended: true }))
-  app.use( express.static( __dirname + '/public' ) );
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
+  app.use( express.static( __dirname + '/public' ) );
   
   // Get default schema
   app.get('/getDefaultSchema',function(req,res){
-      let schemaFile = JSON.parse(fs.readFileSync("schema.json", 'utf8'));
-      res.send(schemaFile.Default)
-  
+    console.log("Getting default schema")
+    let schemaFile = JSON.parse(fs.readFileSync("schema.json", 'utf8'));
+    res.send(schemaFile.Default);
+    
   });
   
   // Get schema of project
   app.post('/getProjectSchema',function(req,res){
+    try {
+      console.log("Getting "+req.body.projectName+" Schema");
+      let schema = {};
+      schema[req.body.projectName] = {};
       
-      try {
-          console.log("Getting "+req.body.projectName+" Schema");
-          let schema = {};
-          schema[req.body.projectName] = {};
-          schema[req.body.projectName]._id = "_id";
-          schemaFile = JSON.parse(fs.readFileSync("schema.json", 'utf8'));
-          schema[req.body.projectName] = {...schema[req.body.projectName],...schemaFile[req.body.projectName]}
-          
-          res.send(schema[req.body.projectName])
-      } catch (err) {
-          console.log(err);
-          res.send ({"Error": err});
-      }
+      // Set ID element in schema
+      schema[req.body.projectName]._id = "_id";
       
-  
+      schemaFile = JSON.parse(fs.readFileSync("schema.json", 'utf8'));
+      schema[req.body.projectName] = {...schema[req.body.projectName],...schemaFile[req.body.projectName]};
+      
+      res.send(schema[req.body.projectName]);
+    } catch (err) {
+      console.log(err);
+      res.send ({"Error": err});
+    }
+    
   });
   
   // Edit schema of project 
   app.post('/editProjectSchema',function(req,res){
-      try {
-          let schemaFile = JSON.parse(fs.readFileSync("schema.json", 'utf8'));
-          let oldProjectName = req.body.oldProjectName;
-          let newProjectName = req.body.newProjectName;
-          // Rename project
-          if (oldProjectName != newProjectName) {
-              MongoDatabase.db("IssueTracker").collection(oldProjectName).rename(newProjectName);
-              schemaFile[newProjectName] = schemaFile[oldProjectName];
-              delete schemaFile[oldProjectName];
-          }
-          
-          // Edit schema
-          console.log("Editing "+newProjectName+" Schema")
-          res.send("Editing Schema");
-          let schema = {};
-          schema["_id"] = {"type":"_id"};
-          delete schemaFile[newProjectName]["_id"];
-          let oldSchema = schemaFile[newProjectName];
-          console.log(oldSchema)
-
-          
-          let oldSchemaKeys = Object.keys(oldSchema);
-          let submittedSchema = req.body.schema;
-          console.log(submittedSchema)
-          let submittedSchemaKeys = Object.keys(submittedSchema);
-          for (let i=0;i<submittedSchemaKeys.length;i++) {
-            if (submittedSchema[submittedSchemaKeys[i]].type!="Multiple Choice") {
-              schema[submittedSchemaKeys[i]] = submittedSchema[submittedSchemaKeys[i]];
-            } else {
-              schema[submittedSchemaKeys[i]] = {"type":"Multiple Choice"};
-              if (i<oldSchemaKeys.length) {
-                  
-                  console.log("SchemaKeys " + oldSchemaKeys[i])
-                  console.log("OldSchemaKeys " +oldSchema[oldSchemaKeys[i]].options)
-                  schema[submittedSchemaKeys[i]].options = oldSchema[oldSchemaKeys[i]].options;
-              } else {
-                  schema[submittedSchemaKeys[i]].options = [];
-              }
-              
-              for (let j=0;j<submittedSchema[submittedSchemaKeys[i]].newOptions.length;j++) {
-                  if (submittedSchema[submittedSchemaKeys[i]].newOptions[j]!="") {
-                      schema[submittedSchemaKeys[i]].options.push(submittedSchema[submittedSchemaKeys[i]].newOptions[j])
-                  }
-                  
-              }
-            }
-            
-              
-          }
-          
-          
-          if (schemaFile.hasOwnProperty(newProjectName)) {
-              setSchema(newProjectName,schema)
-          }
-  
-          for (let j=0; j<oldSchemaKeys.length;j++) {
-            if (j<submittedSchemaKeys.length) {
-              if (oldSchemaKeys[j]!=submittedSchemaKeys[j]) {
-                  
-                  MongoDatabase.db("IssueTracker").collection(newProjectName).updateMany({},{$rename:{[oldSchemaKeys[j]]:submittedSchemaKeys[j]}});
-                  
-                  
-              }
-            }
-          }
-
-        console.log(req.body.deleteOldProperties)
-          if (req.body.deleteOldProperties==true) {
-            for (let j=0; j<req.body.schemaIDsToDelete.length;j++) {
-              schemaIDToDelete = req.body.schemaIDsToDelete[j];
-              console.log(schemaIDToDelete)
-              MongoDatabase.db("IssueTracker").collection(newProjectName).updateMany({},{ $unset: {[schemaIDToDelete]:""} }
-  )
-            }
-          }
-          
-          
-      } catch(err) {
-          console.log(err);
-          res.send ({"Error": err});
+    try {
+      // Define vars
+      let schemaFile = JSON.parse(fs.readFileSync("schema.json", 'utf8'));
+      let oldProjectName = req.body.oldProjectName;
+      let newProjectName = req.body.newProjectName;
+      
+      // Rename project if necessary
+      if (oldProjectName != newProjectName) {
+          MongoDatabase.db("IssueTracker").collection(oldProjectName).rename(newProjectName);
+          schemaFile[newProjectName] = schemaFile[oldProjectName];
+          delete schemaFile[oldProjectName];
       }
+      
+      // Edit schema
+      console.log("Editing "+newProjectName+" Schema")
+      res.send("Editing Schema");
+
+      // Setup vars and define ID element
+      let schema = {};
+      schema["_id"] = {"type":"_id"};
+      delete schemaFile[newProjectName]["_id"];
+      let oldSchema = schemaFile[newProjectName];
+      let oldSchemaKeys = Object.keys(oldSchema);
+      let submittedSchema = req.body.schema;
+      let submittedSchemaKeys = Object.keys(submittedSchema);
+
+      // Loop through schema and process based on type
+      for (let i=0;i<submittedSchemaKeys.length;i++) {
+        if (submittedSchema[submittedSchemaKeys[i]].type!="Multiple Choice") {
+          schema[submittedSchemaKeys[i]] = submittedSchema[submittedSchemaKeys[i]];
+        } else {
+          schema[submittedSchemaKeys[i]] = {"type":"Multiple Choice"};
+          
+          if (i<oldSchemaKeys.length) {
+            schema[submittedSchemaKeys[i]].options = oldSchema[oldSchemaKeys[i]].options;
+          } else {
+            schema[submittedSchemaKeys[i]].options = [];
+          }
+          
+          for (let j=0;j<submittedSchema[submittedSchemaKeys[i]].newOptions.length;j++) {
+            if (submittedSchema[submittedSchemaKeys[i]].newOptions[j]!="") {
+                schema[submittedSchemaKeys[i]].options.push(submittedSchema[submittedSchemaKeys[i]].newOptions[j])
+            }
+          }
+        } 
+      }
+      
+      // Set schema if it exists
+      if (schemaFile.hasOwnProperty(newProjectName)) {
+        setSchema(newProjectName,schema);
+      }
+
+      // Rename properties in database if required
+      for (let j=0; j<oldSchemaKeys.length;j++) {
+        if (j<submittedSchemaKeys.length) {
+          if (oldSchemaKeys[j]!=submittedSchemaKeys[j]) {     
+            MongoDatabase.db("IssueTracker").collection(newProjectName).updateMany({},{$rename:{[oldSchemaKeys[j]]:submittedSchemaKeys[j]}});
+          }
+        }
+      }
+
+      // Delete properties in database if required
+      if (req.body.deleteOldProperties==true) {
+        for (let j=0; j<req.body.schemaIDsToDelete.length;j++) {
+          schemaIDToDelete = req.body.schemaIDsToDelete[j];
+          MongoDatabase.db("IssueTracker").collection(newProjectName).updateMany({},{$unset: {[schemaIDToDelete]:""}});
+        }
+      }
+      
+    } catch(err) {
+      console.log(err);
+      res.send ({"Error": err});
+    }
+    
   });
   
   // Get list of project names
   app.get('/getProjectNames',function(req,res){
-      console.log("Getting Project Names");
-      getProjectNames().then((value)=> {
-          res.send(value)
-      });
+    console.log("Getting Project Names");
+    getProjectNames().then((value)=> {
+        res.send(value);
+    });
       
   });
   
-  // Create new project in schema and in database (potential to break if custom request is run)
+  // Create new project in schema and in database 
   app.post('/createNewProject',function(req,res){
       try {
-          let updatedTime = (new Date(Date.now())).toString();
-          
           console.log("Creating New Project "+req.body.projectName);
+          let updatedTime = (new Date(Date.now())).toString();
           createNewProject(req.body.projectName);
           createNewIssue(req.body.projectName,{"projectTimeEditedExists":"true","projectTimeEdited":updatedTime});
           setSchema(req.body.projectName,req.body.schema);
